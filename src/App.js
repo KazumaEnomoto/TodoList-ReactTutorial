bf713aacb0a98fc0bfa9a2e4176e91ddd99ba5dc
榎本一舞 <kazumaenomoto@MacBook-Air.local>
2023-08-04 08:32:53 +0900

    TodoListの要素を作成

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ function App() {
   return (
     <div>
       <TodoList />
+      <input type="text" />
+      <button>タスクの追加</button>
+      <button>完了したタスクの削除</button>
+      <div>残りのタスク:0</div>
     </div>
   );
 }
